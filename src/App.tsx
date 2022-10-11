ac683edf090868ce7401fb7123fa8063d2786c1b
wojtekwro29 <wojtekwro2006@wp.pl>
2022-10-11 23:08:47 +0200

    Add Help Form Design

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,31 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import WithAuth from "./components/WithAuth/WithAuth";
 import WithoutAuth from "./components/WithoutAuth/WithoutAuth";
+import AddHelpRequest from "./AddHelpRequest";
 
 const theme = extendTheme({
-  initialColorMode: "dark",
-  useSystemColorMode: false,
+    initialColorMode: "dark",
+    useSystemColorMode: false,
 });
 
 function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={"dark"} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="auth" element={<WithoutAuth />}>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-          </Route>
-          <Route path="*" element={<WithAuth />}>
-            <Route path="" element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={"dark"} />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="auth" element={<WithoutAuth />}>
+                        <Route path="login" element={<Login />} />
+                        <Route path="register" element={<Register />} />
+                    </Route>
+                    <Route path="*" element={<WithAuth />}>
+                        <Route path="" element={<Home />} />
+                        <Route path="addpost" element={<AddHelpRequest />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
+    );
 }
 
 export default App;

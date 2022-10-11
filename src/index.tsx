e45fb07236efc5ae4a5a5e27df3f3f389315d80e
Arciiix <artur.nowak100@gmail.com>
2022-10-11 23:48:09 +0200

    Force dark mode

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,8 @@ root.render(
   </React.StrictMode>
 );
 
+localStorage.setItem("chakra-ui-color-mode", "dark");
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

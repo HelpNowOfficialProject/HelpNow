8397860995baf02e9539d12115f42bdb4cc11ab7
Arciiix <artur.nowak100@gmail.com>
2022-10-14 22:31:18 +0200

    Add some margin in the app bar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,11 @@
 import {
-    Box,
-    Button,
-    Container,
-    Flex,
-    Heading,
-    IconButton,
-    Tooltip,
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  IconButton,
+  Tooltip,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import HelpList from "../HelpList/HelpList";
@@ -13,32 +13,32 @@ import LogOut from "../LogOut/LogOut";
 import { FaPlus } from "react-icons/fa";
 
 export default function Home() {
-    return (
-        <Container minW={`100%`} mt={`10px`}>
-            <Flex w="100%" justifyContent="space-between">
-                <Heading>HelpNow</Heading>
-                <Flex gap="2px" alignItems="center">
-                    <Box>
-                        <Tooltip label="Dodaj post">
-                            <Link to="post/add">
-                                <IconButton
-                                    aria-label="Dodaj post"
-                                    icon={<FaPlus />}
-                                    // size={"lg"}
-                                    as={Button}
-                                />
-                            </Link>
-                        </Tooltip>
-                    </Box>
-                    <Flex flexDir={`row`}>
-                        {/* <Box><Button mr={`10px`}>Dodaj post</Button></Box> */}
-                        <Box textAlign={`right`}>
-                            <LogOut />
-                        </Box>
-                    </Flex>
-                </Flex>
-            </Flex>
-            <HelpList />
-        </Container>
-    );
+  return (
+    <Container minW={`100%`} mt={`10px`}>
+      <Flex w="100%" justifyContent="space-between">
+        <Heading>HelpNow</Heading>
+        <Flex gap="10px" alignItems="center">
+          <Box>
+            <Tooltip label="Dodaj post">
+              <Link to="post/add">
+                <IconButton
+                  aria-label="Dodaj post"
+                  icon={<FaPlus />}
+                  // size={"lg"}
+                  as={Button}
+                />
+              </Link>
+            </Tooltip>
+          </Box>
+          <Flex flexDir={`row`}>
+            {/* <Box><Button mr={`10px`}>Dodaj post</Button></Box> */}
+            <Box textAlign={`right`}>
+              <LogOut />
+            </Box>
+          </Flex>
+        </Flex>
+      </Flex>
+      <HelpList />
+    </Container>
+  );
 }
